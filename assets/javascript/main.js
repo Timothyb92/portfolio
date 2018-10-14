e7a32f7fa8a6c226a0837ffdf33d9b26e30797ef
Timothy Bedsole <timbedsole@gmail.com>
2018-10-14 11:24:13 -0400

    Added transition styling to navbar

---
+++ assets/javascript/main.js
@@ -0,0 +1,16 @@
+window.onload = () =>  {
+  window.onscroll = () => stickyNav();
+  
+  const header = document.querySelector('.header');
+  const navbar = document.querySelector('.navbar');
+  
+  const sticky = header.offsetTop;
+  
+  function stickyNav() {
+    if (window.pageYOffset > sticky) {
+      navbar.classList.add('sticky');
+    } else {
+      navbar.classList.remove('sticky');
+    }
+  }
+}
